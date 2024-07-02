396687a7dc28cd293317aca2ce984d2376c4f763
FayNguyen03 <ntkhanh391914@gmail.com>
2024-07-01 23:18:09 -0500

    Added final code

--- letter.js
+++ letter.js
@@ -1,30 +1,41 @@
 class Letter{
     constructor(){
         this.t = random(alphabets);
-        this.x0 = width/2;
-        this.y0 = 0;
-        this.size = 50;
-
-        let bbox = font.textBounds(this.t, this.x0, this.y0, this.size);
-        this.x = bbox.x;
-        this.y = bbox.y;
-        this.w = bbox.w;
-        this.h = bbox.h;
-        this.dy = 1;
+        this.reset();
     }
 
     update(x, y ,w, h){ 
         if (this.collide(x, y, w, h) == false){
             this.y += this.dy;
+            this.c = 0;
+        }
+        else{
+            this.c = color(255,100,0);
+        }
+
+        if (this.y + this.h > height){
+            this.reset();
         }
     }
     
+    reset(){
+        this.x0 = random(width);
+        this.y0 = random(-50, -10);
+        this.size = random(15,30);
+        this.c = color(0)
+        let bbox = font.textBounds(this.t, this.x0, this.y0, this.size);
+        this.x = bbox.x;
+        this.y = bbox.y;
+        this.w = bbox.w;
+        this.h = bbox.h;
+        this.dy = map(this.size, 15, 30, 0.4, 2);
+    }
     display(){
         noFill();
-        stroke(0);
-        rect(this.x, this.y , this.w, this.h);
+        noStroke();
+        //rect(this.x, this.y , this.w, this.h);
 
-        fill(0);
+        fill(this.c);
         textFont(font);
         textSize(this.size);
         text(this.t, this.x, this.y + this.h);
@@ -36,7 +47,7 @@ class Letter{
         let top = y;
         let bottom = y + h;
         if (right >= this.x && 
-            left <= this.w + this.h && 
+            left <= this.w + this.x && 
             top <= this.y + this.h && 
             bottom >=  this.y + this.h){
             return true;
--- sketch.js
+++ sketch.js
@@ -1,27 +1,32 @@
 let font;
 let x0, y0;
-let x, y = 150, w = 90, h = 5;
-let size = 100;
+let x;
+let y = 150;
+let w = 90;
+let h = 5;
 let alphabets = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
   "A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Y", "Z"]
-let l;
+let letters = [];
+let nums = 100;
 
 function preload(){
   font = loadFont("fonts/Sushi Sushi.ttf");
 }
 
 function setup() {
-  createCanvas(400, 400);
-  x0 = width / 2;
-  y0 = height / 2;
-  l = new Letter();
-
+  createCanvas(1000, 600);
+  for (let i = 0; i < nums; i++){
+    letters[i] = new Letter();
+  }
 }
 
 function draw() {
   background(220);
   x = mouseX;
-  rect(x, y , w, h);
-  l.update(x, y , w, h);
-  l.display();
+  fill(255);
+  rect(x, y, w, h);
+  for (let i = 0; i < nums; i++){
+    letters[i].update(x, y, w, h);
+    letters[i].display();
+  }
 }
